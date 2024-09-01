e874122c82bacff02781d96d00fc61dc6d63c6b1
Denis <dbreznikov@gmail.com>
2024-09-01 17:05:42 +0300

    Add compare method in MyArrayList:

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,14 +5,20 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        MyArrayList<String> strings = new MyArrayList<String>(Arrays.asList("Hello", "Test", "Desert", "Happy"));
-        MyArrayList<String> strings2 = new MyArrayList<String>();
-
+        MyArrayList<String> strings = new MyArrayList<>(Arrays.asList("Hello", "Test", "Desert", "Happy"));
+        MyArrayList<Integer> nums = new MyArrayList<>(Arrays.asList(4, 1, 3, 8));
         System.out.println(strings);
+        System.out.println(nums);
+        strings.sort();
+        nums.sort();
+
+
+        MyArrayList<String> strings2 = new MyArrayList<String>();
         //        System.out.println(strings2.length());
         //        strings2.addWithOffset(0, "hello");
         //        System.out.println(strings2);
 
         ArrayList<String> arrayList = new ArrayList<>();
+
     }
 }
--- src/main/java/org/example/MyArrayList.java
+++ src/main/java/org/example/MyArrayList.java
@@ -3,11 +3,12 @@ package org.example;
 import java.io.Serializable;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Comparator;
 
 /**
  *
  */
-public class MyArrayList<E> implements Serializable {
+public class MyArrayList<E> implements Serializable, Comparator<E> {
     transient E[] arr;
     private final transient Object[] DEFAULT_CAPACITY_EMPTY_ARR = {};
     final int DEFAULT_CAPACITY_INCREASE = 1;
@@ -104,8 +105,123 @@ public class MyArrayList<E> implements Serializable {
         return arr[i];
     }
 
+    public void clear() {
+        this.arr = (E[]) DEFAULT_CAPACITY_EMPTY_ARR;
+    }
+
     @Override
     public String toString() {
         return Arrays.toString(this.arr);
+    }
+
+    public void sort() {
+        int middleIndex = this.length() / 2;
+        E pivot = this.get(middleIndex);
+        this.delete(middleIndex);
+        MyArrayList<E> arrLeft = new MyArrayList<>();
+        MyArrayList<E> arrRight = new MyArrayList<>();
+
+        for (int i = 0; i < this.length(); i++) {
+            E element = this.get(i);
+            if (compare(element, pivot) == -1) {
+                arrLeft.add(element);
+            }
+        }
+
+        for (int i = 0; i < this.length(); i++) {
+            E element = this.get(i);
+            if (compare(element, pivot) == 1) {
+                arrRight.add(element);
+            }
+        }
+    }
+
+    @Override
+    public int compare(E o1, E o2) {
+        boolean isString = o1 instanceof String;
+        boolean isInt = o1 instanceof Integer;
+        boolean isDouble = o1 instanceof Double;
+        boolean isFloat = o1 instanceof Float;
+        boolean isByte = o1 instanceof Byte;
+        boolean isChar = o1 instanceof Character;
+        boolean isShort = o1 instanceof Short;
+        boolean isLong = o1 instanceof Long;
+        boolean isBoolean = o1 instanceof Boolean;
+
+        if (isString) {
+            return compareString(o1, o2);
+        } else if (isInt) {
+            return compareInt(o1, o2);
+        } else if (isDouble) {
+            return compareDouble(o1, o2);
+        } else if (isFloat) {
+            return compareFloat(o1, o2);
+        } else if (isByte) {
+            return compareByte(o1, o2);
+        } else if (isChar) {
+            return compareChar(o1, o2);
+        } else if (isShort) {
+            return compareShort(o1, o2);
+        } else if (isLong) {
+            return compareLong(o1, o2);
+        } else if (isBoolean) {
+            return compareBoolean(o1, o2);
+        } else {
+            return -2;
+        }
+    }
+
+    public int compareString(E o1, E o2) {
+        String str1 = (String) o1;
+        String str2 = (String) o2;
+        return str1.compareTo(str2);
+    }
+
+    public int compareInt(E o1, E o2) {
+        int int1 = (int) o1;
+        int int2 = (int) o2;
+        return Integer.compare(int1, int2);
+    }
+
+    public int compareDouble(E o1, E o2) {
+        double double1 = (double) o1;
+        double double2 = (double) o2;
+        return Double.compare(double1, double2);
+    }
+
+    public int compareFloat(E o1, E o2) {
+        float float1 = (float) o1;
+        float float2 = (float) o2;
+        return Float.compare(float1, float2);
+    }
+
+    public int compareByte(E o1, E o2) {
+        byte byte1 = (byte) o1;
+        byte byte2 = (byte) o2;
+        return Byte.compare(byte1, byte2);
+    }
+
+    public int compareChar(E o1, E o2) {
+        char char1 = (char) o1;
+        char char2 = (char) o2;
+        return Character.compare(char1, char2);
+    }
+
+    public int compareShort(E o1, E o2) {
+        short short1 = (short) o1;
+        short short2 = (short) o2;
+        return Short.compare(short1, short2);
+    }
+
+    public int compareLong(E o1, E o2) {
+        long long1 = (long) o1;
+        long long2 = (long) o2;
+        return Long.compare(long1, long2);
+    }
+
+    public int compareBoolean(E o1, E o2) {
+        boolean boolean1 = (boolean) o1;
+        boolean boolean2 = (boolean) o2;
+        return Boolean.compare(boolean1, boolean2);
     }
 }
