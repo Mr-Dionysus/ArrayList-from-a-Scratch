969c02799d6d7a5ee936529a631bf9d79babcbf3
Denis <dbreznikov@gmail.com>
2024-09-02 16:28:55 +0300

    fix: QuickSort reference at Person class didn't work

--- src/main/javadoc/type-search-index.js
+++ src/main/javadoc/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.example","l":"Main"},{"p":"org.example","l":"MyArrayList"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.example","l":"Main"},{"p":"org.example","l":"MyArrayList"},{"p":"org.example","l":"Person"}];updateSearchResults();
--- src/test/java/org/example/Person.java
+++ src/test/java/org/example/Person.java
@@ -1,8 +1,7 @@
 package org.example;
 
 /**
- * Used as a test class in the <b>QuickSort</b> method
- * {@link org.example.MyArrayList#quickSort(java.util.Comparator<E>)}. Have height with which you
+ * Used as a test class in the <b>QuickSort</b> method {@link org.example.MyArrayList#quickSort(java.util.Comparator)}. Have height with which you
  * can compare <b>Person</b> objects.
  */
 public class Person {
